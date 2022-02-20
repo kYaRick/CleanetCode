2f5174c5bb1c0f9231da06f5d3b07be76bad60c9
Ярослав Килюшик <3drpuk@gmail.com>
2022-02-20 16:25:29 +0200

    Second homework was added.

---
+++ TypesBranchesAndLoops/TypesBranchesAndLoops/kYaCleanNETCode/MysteryNumber.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using static System.Console;
+
+namespace kYaCleanNETCode
+{
+    public class MysteryNumber
+    {
+        public sbyte AttemptsNum { get; private set; } = 3;
+        
+        private readonly string _ChatPrefix = "[MysteryNumber]";
+        private ushort _GivenNum = 42;
+
+        public MysteryNumber(){}
+        public MysteryNumber(string chatPrefix, sbyte attemptsNum)
+        {
+            _ChatPrefix = chatPrefix;
+            AttemptsNum = attemptsNum;
+        }
+
+        public ushort GetRandomInScope(ushort min, ushort max, bool isShowMessage = true)
+        {
+            if (isShowMessage)
+            {
+                WriteLine($"{_ChatPrefix} I'm thinking of a number between {min} and {max}...");
+                WriteLine($"{_ChatPrefix} Try to think what number I'm thinking of?");
+            }
+
+            return _GivenNum = (ushort)new Random().Next(min, ++max);
+        }
+        public bool TryToGuess(ushort inputNum, bool isShowMessage = true) 
+        {
+            bool isUserWin = false;
+
+            if (AttemptsNum > 0 || AttemptsNum != -1)
+            {
+                if (inputNum == _GivenNum)
+                {
+                    isUserWin = true;
+                    if (isShowMessage)
+                    {
+                        WriteLine($"{_ChatPrefix} Welcome! You won!");
+                    }
+                }
+                else
+                {
+                    if (isShowMessage)
+                    {
+                        WriteLine($"{_ChatPrefix} So... {inputNum} it's not bad, but... This is mistake");
+                    }
+                }
+            } 
+            else
+            {
+                WriteLine($"{_ChatPrefix} Your attempts are over");
+            }
+
+            AttemptsNum--;
+            return isUserWin;
+        }
+    }
+}
---
+++ TypesBranchesAndLoops/TypesBranchesAndLoops/kYaCleanNETCode/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using static System.Console;
+
+using kYaCleanNETCode;
+
+namespace TypesBranchesAndLoops
+{
+    class Program
+    {
+        //Configuration block:
+        private static string _ChatPrefix = "[MysteryNumber]";
+        private static sbyte _AttemptsNum = 5;
+        private static ushort _MinRND = 1;
+        private static ushort _MaxRND = 3;
+        private static string _ProgramPrefix = "[Program]";
+        private static string _UserPrefix = "[User]";
+        //
+        
+        private static string? userName;
+        public static string? UserName { 
+            get
+            {
+                if (string.IsNullOrWhiteSpace(userName))
+                {
+                    return "User name didn't install";
+                }
+                else
+                {
+                    return userName;
+                }
+            }
+            set 
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    userName = "";
+                } 
+                else
+                {
+                    _UserPrefix = '[' + value + ']';
+                    userName = value;
+                }
+            } }
+
+        public static void Main()
+        {
+            var isContinue = false;
+
+            WriteLine($"{_ProgramPrefix} Enter You name, please. (\"Enter\" for skip)");
+            Write($"{_UserPrefix} ");
+            UserName = ReadLine();
+
+            do
+            {
+                var mn = new MysteryNumber(_ChatPrefix, _AttemptsNum);
+                mn.GetRandomInScope(_MinRND, _MaxRND);
+                bool isUserWin = false;
+                while ((mn.AttemptsNum > 0 || mn.AttemptsNum == -1) && !isUserWin)
+                {
+                    Write($"{_UserPrefix} [{mn.AttemptsNum}\\{_AttemptsNum}] Enter You num: ");
+
+                    ushort.TryParse(ReadLine(), out ushort inputNum);
+                    isUserWin = mn.TryToGuess(inputNum);
+                }
+
+                WriteLine("So...\n\t1 - start over\n\t0 - exit");
+                Write($"{_UserPrefix}");
+                var keyCode= ReadKey().Key;
+                switch (keyCode)
+                {
+                    case ConsoleKey.D1:
+                        {
+                            isContinue = true;  
+                            Clear();
+                        }break;
+                    default:
+                        {
+                            isContinue = false;
+                            WriteLine($"Bye {UserName}!");
+                        }
+                        break;
+                }
+            } while (isContinue);
+            ReadKey();
+        }
+    }
+}
